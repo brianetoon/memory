def14e8a73c41cca13fa20ba43bb68338042df6f
Brian Toon <brian.elliot.toon@gmail.com>
2024-12-18 22:52:07 -0700

    render board with shuffled card images

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ const board = document.getElementById("board");
 const form = document.querySelector(".user-form");
 const currentPlayer = document.querySelector(".user-form");
 const usernameInput = form.firstElementChild;
+const boardStagger = 75;
+
+const cardImages = [
+  "banana",
+  "brownlab",
+  "corgi",
+  "goldenpuppy",
+  "greenpug",
+  "husky",
+  "kingcharles",
+  "sleepy",
+  "window",
+  "yawn"
+];
 
 
 form.addEventListener("submit", e => {
@@ -17,13 +31,45 @@ form.addEventListener("submit", e => {
 });
 
 function renderBoard() {
-  for (let i = 0; i < 16; i++) {
+  // call function to return randomized array of the 8 image pairs
+  const cards = getShuffledPairs(cardImages);
+
+  for (let i = 0; i < cards.length; i++) {
     const card = document.createElement("div");
     card.classList.add("card");
+    card.style.backgroundImage = `url("/images/dogs/${cards[i]}.jpg")`;
+    card.setAttribute("name", cards[i]);
 
     setTimeout(() => {
       board.appendChild(card);  
-    }, 50 * i);
+    }, boardStagger * i);
   }
+
+  // prevent player from clicking on card until all cards are rendered
+  setTimeout(() => {
+    board.addEventListener("click", e => {
+      if (e.target.classList.contains("card")) {
+        handleClick(e.target);
+      }
+    });
+  }, boardStagger * cards.length);
 }
 
+function handleClick(card) {
+  console.log(card.getAttribute("name"));
+}
+
+function getShuffledPairs(arr) {
+  const shuffledArr = shuffle(arr);
+  const slicedArr = shuffledArr.slice(0, 8);
+  const pairedArr = [...slicedArr, ...slicedArr];
+  const shuffledPairs = shuffle(pairedArr);
+  return shuffledPairs;
+}
+
+function shuffle(arr) {
+  const shuffledArr = arr.sort(() => Math.random() - 0.5);
+  return shuffledArr;
+}
+
+// console.log(getShuffledPairs(cardImages));
